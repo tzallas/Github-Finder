aafd8589f925bb282da9d5dc679b2163c3b797bf
tzallas <tzallasilias@gmail.com>
2019-06-15 14:52:53 +0300

    UserItem is ready

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class UserItem extends Component {
+const UserItem = (props) => {
+    const { login, avatar_url, html_url } = props.user
+    return (
+        <div className="card text-center">
+            <img
+                src={avatar_url}
+                alt=""
+                className="round-img"
+                style={{ width: '60px' }} />
+            <h3>{login}</h3>
 
-    render() {
-        return (
-            <div className="card text-center">
-                <img
-                    src={this.props.user.avatar_url}
-                    alt=""
-                    className="round-img"
-                    style={{ width: '60px' }} />
-                <h3>{this.props.user.login}</h3>
-
-                <div>
-                    <a href={this.props.user.html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default UserItem
