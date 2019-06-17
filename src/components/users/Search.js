a0cb8527c53aaa3c0a73084e64c6ccfad0c51a80
tzallas <tzallasilias@gmail.com>
2019-06-17 21:35:19 +0300

    form onChange onSubmit ready in Search.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    onSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.text);
+    }
+
     render() {
         return (
             <div>
-                <form className="form">
-                    <input type="text" name="text" placeholder="Search Users..." />
-                    <input type="submit" value="Search" className="btn btn-dark btn-" />
+                <form onSubmit={this.onSubmit} className="form">
+                    <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
+                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
             </div>
         )
