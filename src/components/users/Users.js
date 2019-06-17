24b8044cb7176fde50daa225914e3c72fd75e479
tzallas <tzallasilias@gmail.com>
2019-06-17 18:57:11 +0300

    Environment variables is ready

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
+import PropTypes from 'prop-types';
 
 const Users = (props) => {
     if (props.loading) {
@@ -22,6 +23,11 @@ const userStyle = {
     display: "grid",
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: "1rem"
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 export default Users
