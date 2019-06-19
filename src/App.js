fc2ea88e286e180bf0895edf798742d3358b5043
tzallas <tzallasilias@gmail.com>
2019-06-20 00:25:46 +0300

    Set an alert when search is empty

--- src/App.js
+++ src/App.js
@@ -4,26 +4,14 @@ import axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
-
+import Alert from "./components/layout/Alert";
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
-  // async componentDidMount() {
-  //   this.setState({
-  //     loading: true
-  //   })
-
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-  //   client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-  //   this.setState({
-  //     users: res.data, loading: false
-  //   })
-  // }
-
   //Searching for users
   searchUsers = async text => {
     this.setState({ loading: true });
@@ -40,15 +28,23 @@ class App extends React.Component {
     this.setState({ users: [], loading: false })
   }
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false} />
+            showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert} />
           <Users
             loading={this.state.loading}
             users={this.state.users}
