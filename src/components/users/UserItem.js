9d60704ff06960dc053672c11187d02a9b278c32
tzallas <tzallasilias@gmail.com>
2019-06-30 18:09:33 +0300

    functionlity odf project reafy

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const UserItem = (props) => {
-    const { login, avatar_url, html_url } = props.user
+    const { login, avatar_url } = props.user
     return (
         <div className="card text-center">
             <img
